0c0d3872b3ffcf7af43ac356375239b1edc65d74
Gabrielrosasilva <68952301+Gabrielrosasilva@users.noreply.github.com>
2026-03-15 01:02:36 -0300

    atualizando

---
+++ infra/terraform/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 5.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket   = "terraform-state-k8s"
+    key      = "projeto-k8s-ha/terraform.tfstate"
+    region   = "sa-saopaulo-1"
+    
+    endpoint = "https://grcz8lmkhuou.compat.objectstorage.sa-saopaulo-1.oraclecloud.com"
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
