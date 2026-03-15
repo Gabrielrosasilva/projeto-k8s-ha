5617b5e0c44b671c757f4eab2f3f505ff6933c46
Gabrielrosasilva <68952301+Gabrielrosasilva@users.noreply.github.com>
2026-03-15 02:34:32 -0300

    Fix: Ajustando sintaxe do backend S3 para compatibilidade com Terraform 1.5.7

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -11,16 +11,12 @@ terraform {
     key    = "projeto-k8s-ha/terraform.tfstate"
     region = "sa-saopaulo-1"
 
+    # Sintaxe correta para o Terraform 1.5.7
+    endpoint                    = "https://grcz8lmkhuou.compat.objectstorage.sa-saopaulo-1.oraclecloud.com"
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
-    skip_s3_checksum            = true
-    use_path_style              = true
-
-    endpoints = {
-      s3 = "https://grcz8lmkhuou.compat.objectstorage.sa-saopaulo-1.oraclecloud.com"
-    }
+    force_path_style            = true
   }
 }
 
